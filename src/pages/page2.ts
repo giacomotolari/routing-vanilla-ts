210290bc3e93f644b32a481b5db27b3a38334cd9
giacomotolari <tolarigiacomo@gmail.com>
2025-01-10 10:58:04 +0100

    feat: add page1, page2, and page3 components

---
+++ src/pages/page2.ts
@@ -0,0 +1,9 @@
+function page2() {
+    return `
+      <div>
+        <h1>Page 2</h1>
+      </div>
+    `;
+  }
+  
+  export default page2;
---
+++ src/pages/page3.ts
@@ -0,0 +1,9 @@
+function page3() {
+  return `
+      <div>
+        <h1>Page 3</h1>
+      </div>
+    `;
+}
+
+export default page3;
